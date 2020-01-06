c4be93d3cb6402222b61851233b4eea907f4ebf9
alexlee-dev <alex@alexlee.dev>
2020-01-06 12:17:08 -0800

    OMDB API integration

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Search from "./Search";
+import axios from "axios";
 
 const Body = () => {
   const [searchValue, setSearchValue] = useState("");
+  const [results, setResults] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (searchValue) {
+      axios({
+        method: "GET",
+        url: `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}&s=${searchValue}`
+      }).then(res => {
+        const { data } = res;
+        const { Response } = data;
+        if (Response === "True") {
+          setError(null);
+          setResults(data);
+        } else if (Response === "False") {
+          setError(data.Error);
+        } else {
+          setError(JSON.stringify({ data }, null, 2));
+        }
+      });
+    }
+  }, [searchValue]);
   return (
     <div>
       <Search searchValue={searchValue} setSearchValue={setSearchValue} />
-      <pre>{JSON.stringify({ searchValue }, null, 2)}</pre>
+      {error && <span>{error}</span>}
+      {!error && <pre>{JSON.stringify({ results }, null, 2)}</pre>}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "./app.css";
 import * as serviceWorker from "./serviceWorker";
+require("dotenv").config();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
